122a5d3fed0c003400460962dc5cbb8b41a15373
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 13:15:02 +0300

    Update Task5.php

--- Task5.php
+++ Task5.php
@@ -2,7 +2,96 @@
 
 namespace src;
 
+header('Content-type: text/plain');
+use JetBrains\PhpStorm\Pure;
+
 class Task5
 {
+    public static function findSum($str1, $str2): string
+    {
+        if (strlen($str1) > strlen($str2)) {
+            $temp = $str1;
+            $str1 = $str2;
+            $str2 = $temp;
+        }
 
+        $str3 = '';
+
+        $n1 = strlen($str1);
+        $n2 = strlen($str2);
+        $diff = $n2 - $n1;
+
+        $carry = 0;
+
+        for ($i = $n1 - 1; $i >= 0; $i--) {
+            $sum = ((ord($str1[$i]) - ord('0')) + ((ord($str2[$i + $diff]) - ord('0'))) + $carry);
+            $str3 .= chr($sum % 10 + ord('0'));
+            $carry = (int)($sum / 10);
+        }
+
+        for ($i = $n2 - $n1 - 1; $i >= 0; $i--) {
+            $sum = ((ord($str2[$i]) - ord('0')) + $carry);
+            $str3 .= chr($sum % 10 + ord('0'));
+            $carry = (int)($sum / 10);
+        }
+
+        if ($carry) {
+            $str3 .= chr($carry + ord('0'));
+        }
+
+        return strrev($str3);
+    }
+
+    public static function numberOfDigits($n): float|int
+    {
+        if ($n == 1) {
+
+            return 1;
+        }
+
+        // phi = 1.6180339887498948
+        $d = ($n * log10(1.6180339887498948)) - ((log10(5)) / 2);
+
+        return ceil($d);
+    }
+
+    public static function main($n): string
+    {
+        $a = '0';
+        $b = '1';
+        if ($n == 0) {
+            return $a;
+        }
+        for ($i = 2; $i <= $n; $i++) {
+            $c = Task5::findSum($a, $b);
+            $a = $b;
+            $b = $c;
+        }
+
+        return $b;
+    }
+
+
+    #[Pure] public static function fib($n): int
+    {
+        $i = 0;
+        while (true) {
+            if (Task5::numberOfDigits(++$i) >= $n) {
+
+                return $i;
+            }
+        }
+    }
 }
+
+// Тесты
+// Последовательность Фибоначчи
+//for ($i = 0; $i < 20; $i++) {
+//    echo $i . "=>" . Task5::fib($i) . PHP_EOL;
+//}
+
+//echo "First serial number of Fibonacci with length length 2: " . Task5::main(2) . PHP_EOL;
+//echo "First serial number of Fibonacci with length length 3: " . Task5::main(3) . PHP_EOL;
+
+$n = 100;
+Task5::main(Task5::fib($n));
