f5f44fd4bb853e1f4420d8036bffe0d3f02bb5a5
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 11:10:51 +0300

    update

--- Task1.php
+++ Task1.php
@@ -4,24 +4,20 @@ namespace src;
 
 class Task1
 {
-    private function task1(int $inputNumber): string
-    {
-        return $inputNumber > 30 ? 'More than 30'
-            : ($inputNumber > 20 ? 'More than 20'
-            : ($inputNumber > 10 ? 'More than 10'
-            : 'Equal or less than 10'));
-    }
-
-    public function main()
+    public static function main(int $inputNumber): string
     {
         $input = 25; // change value here
         if (!is_int($input)) {
             throw new \InvalidArgumentException('task1 function only accepts integers. Your input was: ' . $input . ' (' . gettype($input) . ')');
         } else {
-            echo $this->task1($input);
+
+            return $inputNumber > 30 ? 'More than 30'
+                : ($inputNumber > 20 ? 'More than 20'
+                : ($inputNumber > 10 ? 'More than 10'
+                : 'Equal or less than 10'));
         }
     }
 }
 
-$task1 = new Task1();
-$task1->main();
+$inputNumber = 25;
+Task1::main($inputNumber);
