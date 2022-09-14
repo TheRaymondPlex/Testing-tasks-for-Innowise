243236b64f711455b4c84cf65e6abfb8f3e0ac63
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 11:15:30 +0300

    Update Task10.php

--- Task10.php
+++ Task10.php
@@ -4,34 +4,28 @@ namespace src;
 
 class Task10
 {
-    private function task10(int $input): array
+    public static function main(int $input): array
     {
-        $resultArray = [$input];
-
-        while ($input > 1) {
-            if ($input % 2 == 0) {
-                $input = $input / 2;
-            } else {
-                $input = $input * 3 + 1;
-            }
-            array_push($resultArray, $input);
-        }
-
-        return $resultArray;
-    }
-
-    public function main()
-    {
-        $number = 12;
-        if (!is_int($number)) {
-            throw new \InvalidArgumentException('Variable $number is not integer! ' . gettype($number) . ' is provided.');
-        } elseif ($number <= 0) {
-            throw new \InvalidArgumentException('Variable $number equals or lower zero! ' . $number . ' is provided.');
+        if (!is_int($input)) {
+            throw new \InvalidArgumentException('Variable $number is not integer! ' . gettype($input) . ' is provided.');
+        } elseif ($input <= 0) {
+            throw new \InvalidArgumentException('Variable $number equals or lower zero! ' . $input . ' is provided.');
         } else {
-            print_r($this->task10($number));
+            $resultArray = [$input];
+
+            while ($input > 1) {
+                if ($input % 2 == 0) {
+                    $input = $input / 2;
+                } else {
+                    $input = $input * 3 + 1;
+                }
+                array_push($resultArray, $input);
+            }
+
+            return $resultArray;
         }
     }
 }
 
-$task10 = new Task10();
-$task10->main();
+$input = 12;
+Task10::main($input);
