f761a7a4d3537f801d7221bc97b33b5f020ea830
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 11:12:36 +0300

    Update Task3.php

--- Task3.php
+++ Task3.php
@@ -4,35 +4,25 @@ namespace src;
 
 class Task3
 {
-    private function task3(int $number): int
+    public static function main(int $number): int
     {
-        $sum = 0;
-
-        while ($number > 0 || $sum > 9) {
-            if ($number == 0) {
-                $number = $sum;
-                $sum = 0;
-            }
-            $sum += $number % 10;
-            $number = intdiv($number, 10);
-        }
-
-        return $sum;
-    }
-
-    public function main()
-    {
-        $n = 5689; // <- enter your number here
-
-        if (!is_int($n)) {
-            throw new \InvalidArgumentException('task3 function only accepts integers. Your input was: ' . $n . ' (' . gettype($n) . ')');
+        if (!is_int($number)) {
+            throw new \InvalidArgumentException('task3 function only accepts integers. Your input was: ' . $number . ' (' . gettype($number) . ')');
         } else {
-            echo 'Your number is ' . $n;
-            echo '<br>';
-            echo 'Last single digit after operations is ' . $this->task3($n);
+            $sum = 0;
+            while ($number > 0 || $sum > 9) {
+                if ($number == 0) {
+                    $number = $sum;
+                    $sum = 0;
+                }
+                $sum += $number % 10;
+                $number = intdiv($number, 10);
+            }
+
+            return $sum;
         }
     }
 }
 
-$task3 = new Task3();
-$task3->main();
+$input = 5689;
+Task3::main($input);
