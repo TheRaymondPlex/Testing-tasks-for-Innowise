caadb4bed46fa51ea84723ae25a1238ff9a86b97
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 17:46:19 +0300

    Update Task1.php

--- Task1.php
+++ Task1.php
@@ -6,11 +6,9 @@ class Task1
 {
     public static function main(int $inputNumber): string
     {
-        $input = 25; // change value here
-        if (!is_int($input)) {
-            throw new \InvalidArgumentException('task1 function only accepts integers. Your input was: ' . $input . ' (' . gettype($input) . ')');
+        if (!is_int($inputNumber)) {
+            throw new \InvalidArgumentException('task1 function only accepts integers. Your input was: ' . $inputNumber . ' (' . gettype($inputNumber) . ')');
         } else {
-
             return $inputNumber > 30 ? 'More than 30'
                 : ($inputNumber > 20 ? 'More than 20'
                 : ($inputNumber > 10 ? 'More than 10'
