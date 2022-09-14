11a5d0b1a48c0ef17e50196511ef056f4a290ea6
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-14 11:14:02 +0300

    Update Task7.php

--- Task7.php
+++ Task7.php
@@ -4,29 +4,19 @@ namespace src;
 
 class Task7
 {
-    private function task7(array $arr, int $position)
+    public static function main(array $arr, int $position): array
     {
-        echo 'Original array: ';
-        var_dump($arr);
-        unset($arr[$position]);
-        echo '<br>';
-        array_splice($arr, 0, 0);
-        echo 'New array: ';
-        var_dump($arr);
-
-        return null;
-    }
-
-    public function main()
-    {
-        $arr = [1,2,3,4,5,6,7,8,9];
         if (!is_array($arr)) {
             throw new \InvalidArgumentException('Invalid data! Only arrays are accepted! Your input was: ' . $arr . ' (' . gettype($arr) . ')');
         } else {
-            echo $this->task7($arr, 3);
+            unset($arr[$position]);
+            array_splice($arr, 0, 0);
+
+            return $arr;
         }
     }
 }
 
-$task7 = new Task7();
-$task7->main();
+$arr = [1,2,3,4,5];
+$n = 3;
+Task7::main($arr, $n);
