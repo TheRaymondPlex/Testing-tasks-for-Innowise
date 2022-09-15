0d111af36480157295c0c2a3c6454c89a1f9dfa3
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-15 11:21:55 +0300

    Update Task3.php

--- Task3.php
+++ Task3.php
@@ -7,7 +7,9 @@ class Task3
     public static function main($number): int
     {
         if (!is_int($number) || $number <= 0) {
-            throw new \InvalidArgumentException('task3 function only accepts positive integers. Your input was: ' . $number . ' (' . gettype($number) . ')');
+            throw new \InvalidArgumentException('Main function accepts positive integers only. Your input was: ' . $number . ' (' . gettype($number) . ')');
+        } elseif ($number > 0 && $number <= 9) {
+            throw new \InvalidArgumentException('Main function accepts two digit integers only . Your input was: ' . $number . ' (' . gettype($number) . ')');
         } else {
             $sum = 0;
             while ($number > 0 || $sum > 9) {
