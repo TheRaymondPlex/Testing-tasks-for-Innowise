5932de72335abc51f4508aab536634e3dc781c7e
TheRaymondPlex <65307716+TheRaymondPlex@users.noreply.github.com>
2022-09-15 12:21:51 +0300

    Update Task7.php

--- Task7.php
+++ Task7.php
@@ -6,8 +6,10 @@ class Task7
 {
     public static function main(array $arr, int $position): array
     {
-        if (!is_array($arr)) {
-            throw new \InvalidArgumentException('Invalid data! Only arrays are accepted! Your input was: ' . $arr . ' (' . gettype($arr) . ')');
+        if (!is_array($arr) || empty($arr)) {
+            throw new \InvalidArgumentException('Invalid data! Only full arrays are accepted!');
+        } elseif ($position <= 0 || $position > (count($arr) - 1)) {
+            throw new \InvalidArgumentException('Invalid position! Your input was: ' . $position . ' (' . gettype($position) . ')');
         } else {
             unset($arr[$position]);
             array_splice($arr, 0, 0);
